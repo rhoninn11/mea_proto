8cc003711c6c17c8a3273d88afa9bf58a2e799c2
rhoninn11 <rhoninn11@gmail.com>
2025-03-23 11:12:54 +0100

    - protobufs from python added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mea_proto
+
+my global protobuf connector
---
+++ proto_gen.py
@@ -0,0 +1,34 @@
+from grpc_tools import protoc
+import os
+
+# add to os path
+def proto_gen(in_dir, out_dir):
+    ls = os.listdir(in_dir)
+    protos = []
+
+    for file in ls:
+        if file.endswith(".proto"):
+            proto = os.path.join(in_dir, file)
+            protos.append(proto)
+    print(f'+++ files: {len(ls)}, proto: {len(protos)}')
+    
+    os.makedirs(out_dir, exist_ok=True)
+    for proto in protos:
+        protoc.main((
+            "",
+            f"-I{in_dir}",
+            f"--python_out={out_dir}",
+            f"--pyi_out={out_dir}",
+            f"--grpc_python_out={out_dir}",
+            proto,
+        ))
+
+
+def script():
+    # relative to root
+    IN_DIR="./proto"
+    OUT_DIR="./src/proto"
+    proto_gen(IN_DIR, OUT_DIR)
+    print("+++ proto sources regenerated")
+
+script()
